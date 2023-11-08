08ba93a2775a663a22c7b2fbd57559993d653b18
qqyfly <qiqinyuan@gmail.com>
2023-11-08 14:39:24 +0100

    Add some code to playground.R

--- Homework/Lab/Lab2/playfround.R
+++ Homework/Lab/Lab2/playfround.R
@@ -2,7 +2,29 @@
 g <- function(x,y)
 {
   ret <- - x^2 - x^2 * y^2 - 2 * x * y + 2 * x + 2
-  return(ret)
+}
+# First derivative
+dg <- function(x){
+  ret <- matrix(data = c(-2 * x -2 * x * y^2 + 2*x  + 2,0,-2 * x^2 -2 * x,0),
+                ncol = 2,nrow = 2)
+}
+# Second derivative
+d2g <- function(x){
+  ret <- matrix(data = c(-2 * y^2,-4 * x - 2,-4 * x - 2,0),
+                ncol = 2,nrow = 2)
+}
+
+newton <- function(x0, eps=0.0001){
+  xt  <- x0
+  xt1 <- x0 + 5
+
+  while(abs(xt-xt1)>eps)
+  {
+    xt1 <- xt
+    xt  <- xt1 - dg(xt1)/d2g(xt1)
+    print(xt) 
+  }
+  xt
 }
 
 x <- seq(-3, 3, by = 0.01)
@@ -20,4 +42,4 @@ for (i in 1:dx){
   }
 }
 
-contour(x,y,gx)
+#newton_result <- newton(x0 = gx,eps=0.0001)
