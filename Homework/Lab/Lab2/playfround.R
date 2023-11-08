293c75bc311246a4a60210b25c8f6658df835eae
qinqi464 <qinqi464@hunn2-112.ad.liu.se>
2023-11-08 14:06:49 +0100

    add answer to question 1a

--- Homework/Lab/Lab2/playfround.R
+++ Homework/Lab/Lab2/playfround.R
@@ -1,11 +1,23 @@
-library(ggplot2)
 
-# Create a data frame
-df <- data.frame(
-  x = c(1:10),
-  y = c(1:10)
-)
+g <- function(x,y)
+{
+  ret <- - x^2 - x^2 * y^2 - 2 * x * y + 2 * x + 2
+  return(ret)
+}
 
-# Create a plot
-g <- ggplot(df, aes(x = x, y = y)) + geom_point()
-plot(g)
+x <- seq(-3, 3, by = 0.01)
+y <- seq(-3, 3, by = 0.01)
+
+dx <- length(x)
+dy <- length(y)
+
+gx <- matrix(rep(NA, dx*dy), nrow=dx,ncol=dy)
+
+for (i in 1:dx){
+  for (j in 1:dy)
+  {
+    gx[i,j] <- g(x[i],y[j])
+  }
+}
+
+contour(x,y,gx)
