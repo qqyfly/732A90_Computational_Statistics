c3441ee8194036ab958302fa10af2d54ddc6b45f
Pierre Qi <qiqinyuan@gmail.com>
2023-11-13 00:27:55 +0000

    update question 1 answer

--- Homework/Lab/Lab2/playground1.R
+++ Homework/Lab/Lab2/playground1.R
@@ -5,57 +5,80 @@
 
 # Function g
 g <- function(x, y) {
-  ret <- - x^2 - x^2 * y^2 - 2 * x * y + 2 * x + 2
+  ret <- -x^2 - x^2 * y^2 - 2 * x * y + 2 * x + 2
   return(ret)
 }
 
 # First derivative
-dg <- function(x) {
-  ret <- matrix(data = c(-2 * x - 2 * x * y^2 + 2 * x  + 2, -2 * x^2 - 2 * x),
+dg <- function(x, y) {
+  ret <- matrix(data = c(-2 * x - 2 * x * y^2 - 2 * y  + 2, 
+                         -2 * x^2 * y - 2 * x),
                 ncol = 1, nrow = 2)
   return(ret)
 }
 # Second derivative
-d2g <- function(x) {
-  ret <- matrix(data = c(-2 * y^2, -4 * x - 2, -4 * x - 2, 0),
+d2g <- function(x, y) {
+  ret <- matrix(data = c(-2 * y^2 + 2, -4 * x * y - 2,
+                         -4 * x * y - 2, -2 * x^2),
                 ncol = 2, nrow = 2)
   return(ret)
 }
 
 # Contour plot using the following code.
-x <- seq(-3, 3, by = 0.01)
-y <- seq(-3, 3, by = 0.01)
+n <- 3
+x <- seq(-n, n, by = 0.01)
+y <- seq(-n, n, by = 0.01)
 dx <- length(x)
 dy <- length(y)
 gx <- matrix(rep(NA, dx * dy), nrow = dx, ncol = dy)
 
-for (i in 1:dx){
-  for (j in 1:dy){
-    gx[i, j] <- g(x[i], y[j])
-  }
-}
-contour(x, y, gx)
+#gx <- sapply(1:length(x))
+#for (i in 1:dx){
+#  for (j in 1:dy){
+#    gx[i, j] <- g(x[i], y[j])
+#  }
+#}
+
+gx <- sapply(y, function(i) sapply(x, function(j) g(i, j)))
+
+#sapply(1:length(x), function(i) func(x[i], y[i]))
+contour(x, y, gx, nlevels = 7, col = "blue")
 ################################################################################
 
 
 ########################## [ 1 b ] #############################################
 # we can get a local maximum of g by using the following code.
 
-newton <- function(x0, eps = 0.0001, max_step = 1000) {
-  xt  <- x0
+distance <- function(xt1, xt0) {
+  ret <- t(xt1 - xt0) %*% (xt1 - xt0)
+  return(ret[1][1])
+}
+
+newton <- function(x0, eps = 1e-10, max_step = 1000) {
+  xt0  <- x0
   xt1 <- x0 + 5
   step <- 1
-  while (base::abs(xt - xt1) > eps) {
+  criterion = 100
+  while (criterion > eps) {
     if (step > max_step) {
       break
+    }else {
+      xt1  <- xt0 - solve(d2g(xt0[1], xt0[2])) %*% dg(xt0[1], xt0[2])
+      old_criterion <- criterion 
+      criterion <- distance(xt1,xt0)
+      
+      cat(criterion,"->")
+      
+      xt0 <- xt1
+      step <- step + 1
     }
-    xt1 <- xt
-    xt  <- xt1 -  solve(d2g(xt1)) %*% dg(xt1)
   }
+ 
   if (step > max_step) {
+    print("The maximum step is reached.")
     return(NA)
   }
-  return(xt)
+  return(xt1)
 }
 ################################################################################
 
@@ -65,25 +88,27 @@ newton <- function(x0, eps = 0.0001, max_step = 1000) {
 # and we get the following results.
 
 # we use the default eps = 0.0001 and max_step here.
-newton_value_1 <- newton(c(2, 0))
-newton_value_2 <- newton(c(-1, 2))
-newton_value_3 <- newton(c(0, -1))
-newton_value_4 <- newton(c(0, 2))
+newton_value_1 <- newton(matrix(c(2, 0), nrow = 2, ncol = 1))
+newton_value_2 <- newton(matrix(c(-1, 2), nrow = 2, ncol = 1))
+newton_value_3 <- newton(matrix(c(0, -1), nrow = 2, ncol = 1))
+newton_value_4 <- newton(matrix(c(0, 2), nrow = 2, ncol = 1))
 
 # We can find the following results.
+
+# It seems that not every initial values will lead to converge.
 
 # Since our newton_value_1 to newton_value_4 are not NA, we can calculate the
 # Gradient vector and Hessian matrix of g at these points listed as table below.
 
-#  |        | Gradient | Hessian |  Type  |
-#  |:---:   |:---:     |:---:    |        |
-#  |(2, 0)  |          |         |        |
-#  |(-1, 2) |          |         |        |
-#  |(0, -1) |          |         |        |
-#  |(0, 2)  |          |         |        |
+#  |        | Gradient | Hessian           |  Type  |
+#  |:---:   |:---:     |:---:              |        |
+#  |(2, 0)  | -2 \\ -4 |  2 & -2 \\ -2 & 8 |        |
+#  |(-1, 2) | 8 \\ -2  |  -6 & 6 \\ 6 & -2 |        |
+#  |(0, -1) | 4 \\ 0   |  0 & -2 \\ -2 & 0 |        |
+#  |(0, 2)  | -2 \\ 0  |  -6 & -2\\ -2 & 0 |        |
 
 # According to the table above and the contour plot, we can find that
-# () are the global maximum if x,y \in [-3, 3] range.
+# point (0,1) are the global maximum if x,y \in [-3, 3] range.
 ################################################################################
 
 ########################## [ 1 d] #############################################
