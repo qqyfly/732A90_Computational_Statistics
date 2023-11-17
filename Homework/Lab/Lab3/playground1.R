a1c42853f904b2cee6ff22ee53391ecffbb9390d
Pierre Qi <qiqinyuan@gmail.com>
2023-11-17 09:34:14 +0100

    1b almost finished

--- Homework/Lab/Lab3/playground1.R
+++ Homework/Lab/Lab3/playground1.R
@@ -3,10 +3,12 @@ rm(list = ls())
 
 ########################## [ 1 a ] #############################################
 # Since we need to find a envelope function for the density function, so we need
-# to plot the density function first. let $ x \in [-3, 3] $
+# to plot the density function first. let $ x \in [-3, 3] $, and we know it's  
+# triangle sharped function. We also can plot it using the code below.
 
 x <- seq(-2, 2, by = 0.001)
 y <- rep(0, length(x))
+z <- rep(0, length(x))
 
 x1_index <- which(x > 1 | x < -1)
 x2_index <- which(x >= -1 & x <= 0)
@@ -16,15 +18,49 @@ y[x1_index] <- 0
 y[x2_index] <- x[x2_index] + 1
 y[x3_index] <- 1 - x[x3_index]
 
-data <- data.frame(x, y)
+z0_index <- which(x > 1 | x < -1)
+z1_index <- which(x >= -1 & x <= 1)
 
-ggplot2::ggplot(data, ggplot2::aes(x = x, y = y)) +
-  ggplot2::geom_line() +
-  ggplot2::ggtitle("Density function of X") +
+z[z1_index] <- 1
+z[-z1_index] <- 0
+
+data <- data.frame(x, y, z)
+
+# After we get the plot, we can see that the density function is a triangle
+# we will use an envelope function to cover the density function, and we can
+# use function $ g(x) = 1 $ to cover the density function in the [-1,1] range. 
+
+graph <- ggplot2::ggplot(data) +
+  ggplot2::geom_line(mapping=ggplot2::aes(x=x,y=y)) + 
+  ggplot2::geom_line(mapping=ggplot2::aes(x=x,y=z),color="blue") + 
+  ggplot2::ggtitle("Density function of X and envelope function") +
   ggplot2::xlab("x") +
-  ggplot2::ylab("y")
+  ggplot2::ylab("y,z")
 
-# After we get the image, we can see that the density function is a triangle
+
+graph
+
+# Now we will write a code for the rejection sampling algorithm. 
 
 
 ########################## [ 1 b ] #############################################
+# To generate a random variable from the density function using Inverse CDF,
+# we will need to calculate the CDF function first.
+
+# CDF function
+
+# in range (-inf,-1) we get CDF = 0
+# in range (-1,0) we get CDF = \frac{x^2}{2} + x
+# in range (0,1)  we get CDF = x - \frac{x^2}{2}
+# in range (1,+inf)  we get CDF = 1
+
+# we have F^{-1} in (-1,0) range is x = -1 + sqrt(1+2y)
+# we have F^{-1} in ( 0,1) range is x =  1 - sqrt(1-2y)
+
+# so we can write a function to generate a random variable from it
+u <- runif(10000)
+x1 <- -1 + sqrt(1+2 * u)
+x2 <- 1 - sqrt(1-2 * u)
+
+hist(x1)
+hist(x2)
--- Homework/Lab/Lab3/playground2.R
+++ Homework/Lab/Lab3/playground2.R
@@ -2,5 +2,28 @@
 rm(list = ls())
 
 ########################## [ 2 a ] #############################################
+# We have the function
+
+#$$
+# DE(\mu,\lambda) = \frac{\lambda}{2} exp(-\lambda|x-\mu|)
+#$$
+
+# Calculate the integral of the function on x to get CDF
+#$$
+#
+#$$
+
+# Calculate the inverse function of CDF
+#$$
+#
+#$$
+
+n <- 1000
+
+# plot histogram of those random variable
+
+# according to the histogram, we can see that the random variable is XXXX
 
 ########################## [ 2 b ] #############################################
+
+n <- 2000
