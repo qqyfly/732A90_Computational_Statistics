71673088839e67e2a9d1d2971a837a618fbf276e
Pierre Qi <qiqinyuan@gmail.com>
2023-11-21 23:05:00 +0100

    update question 1 answer, finished

--- Homework/Lab/Lab3/playground1.R
+++ Homework/Lab/Lab3/playground1.R
@@ -5,6 +5,7 @@ rm(list = ls())
 # Since we need to find a envelope function for the density function, so we need
 # to plot the density function first. let $ x \in [-3, 3] $, and we know it's  
 # triangle sharped function. We also can plot it using the code below.
+# the target function g(x) = 1 in the range [-1,1], otherwise g(x) = 0
 
 x <- seq(-2, 2, by = 0.001)
 y <- rep(0, length(x))
@@ -40,9 +41,47 @@ graph <- ggplot2::ggplot(data) +
 
 graph
 
-# Now we will write a code for the rejection sampling algorithm. 
+# Now we will write a code for the rejection sampling algorithm
+# our g function is g(x) = 1 in the range [-1,1], otherwise g(x) = 0
+# our e function is e(x) = 1 / a in the range [-1,1], otherwise e(x) = 0
+# we will use a = 0.5 here
 
+generate_random_var_1a <- function(n, a) {
+  samples <- numeric(n)
+  # Generate a sample from the proposal distribution in [-1,1]
+  count <- 1
+  e <- 1 / a
 
+  while (count <= n) {
+
+    x <- runif(1, -1, 1)
+    # Calculate the acceptance probability
+    u <- runif(1)
+
+    # since g will always be 1 in the range [-1,1], so y = 1
+    # f1(Y) = 1 - Y and e(Y) = 1 / a = 2
+    # f2(Y) = Y + 1 and e(Y) = 1 / a = 2
+    # so we have f(Y) / e(Y) = (1 - Y) / 2 or Y+1 / e
+
+    # Calculate the acceptance probability
+
+    if (x > 0 && u <= (1 - x) / e) {
+      samples[count] <- x
+      count <- count + 1
+    }
+
+    if (x <= 0 && u <= (x + 1) / e) {
+      samples[count] <- x
+      count <- count + 1
+    }
+  }
+
+  return(samples)
+}
+
+a <- 0.5
+random_var_1a <- generate_random_var_1a(10000, a)
+hist(random_var_1a)
 ########################## [ 1 b ] #############################################
 # To generate a random variable from the density function using Inverse CDF,
 # we will need to calculate the CDF function first.
@@ -54,38 +93,35 @@ graph
 # in range (0,1)  we get CDF = x - \frac{x^2}{2}
 # in range (1,+inf)  we get CDF = 1
 
-# we have F^{-1} in (-1,0) range is x = -1 + sqrt(1-2y)
-# we have F^{-1} in ( 0,1) range is x =  1 - sqrt(1-2y)
+# Since -Y has a triangle  distribution on [-1,0], so we have F^{-1} in [-1,0) is x = -1 + sqrt(1-2y)
+# Also we have F^{-1} in [0,1] range is x =  1 - sqrt(1-2y)
 
-# so we can write a function to generate a random variable from it
-# For x2, sqrt will generate some error because of the negative value
+# The following is the function to generate a random variable from it
 
-u <- runif(1000)
+generate_random_var_1b <- function(n){
+  u <- runif(n)
+  x1 <-  - 1 + sqrt(1 - 2 * u)
+  x2 <-  1 - sqrt(1 - 2 * u)
+  return(c(x1, x2))
+}
 
-x1 <-  sqrt(1 - 2 * u)  - 1
-
-x2 <-  1 - sqrt(1 - 2 * u)
-
-random_var <- c(x1,x2)
-hist(random_var)
-
-# So the random variable is generated from the density function is random_var.
+random_var_1b <- generate_random_var_1b(10000)
+hist(random_var_1b)
 ########################## [ 1 c ] #############################################
-u1_1c <- runif(10000)
-u2_1c <- runif(10000)
+generate_random_var_1c <- function(n){
+  u1_1c <- runif(n)
+  u2_1c <- runif(n)
+  return(u1_1c - u2_1c)
+}
 
-random_var_1c <- u1_1c - u2_1c
+random_var_1c <- generate_random_var_1c(10000)
 hist(random_var_1c)
 
 ########################## [ 1 d ] #############################################
 # Since we already plot the data, we will generate the variable of 1a to 1c
 
 # since the 1c is the simplest way to generate the random variable, but since it 
-# can not adapt to some specific distribution, so we will use 1a to generate the
-# the random variable.
-
-random_var_1c_var <- var(random_var_1c)
-
-
-
+# can not adapt to some specific distribution, for 1b , since we need to calculate 
+# CDF and inverse function, in some cases, it's hard to do that, so we will use 
+# 1a to generate the random variable.
 
