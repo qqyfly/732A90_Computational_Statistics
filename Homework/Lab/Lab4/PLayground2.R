bbcb9d86221d4af9f1662b52bcf9e17d08b7613e
Pierre Qi <qiqinyuan@gmail.com>
2023-11-24 07:55:05 +0100

    update lab4 materials

---
+++ Homework/Lab/Lab4/PLayground2.R
@@ -0,0 +1,9 @@
+# Question 2a
+
+# Question 2b
+
+# Question 2c
+
+# Question 2d
+
+# Question 2e
---
+++ Homework/Lab/Lab4/Playground1.R
@@ -0,0 +1,13 @@
+# Question 1a
+
+
+# Question 1b
+
+# Question 1c
+
+# Question 1d
+
+# Question 1e
+
+# Question 1f
+
