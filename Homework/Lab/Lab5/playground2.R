35bf4ab16f7d438804aa465328148463066d8abf
Pierre Qi <qiqinyuan@gmail.com>
2023-12-12 09:23:57 +0100

    update

--- Homework/Lab/Lab5/playground2.R
+++ Homework/Lab/Lab5/playground2.R
@@ -1,35 +1,15 @@
-
-### Init Code
 rm(list = ls())
 library(ggplot2)
 library(boot)
 
 ################################## (2.1) #######################################
-# DONE
-
 data <- read.csv("./Homework/Lab/Lab5/prices1.csv", sep = ";")
 
 g_2_1 <- ggplot(data, aes(x = SqFt, y = Price)) + geom_point()
-g_2_1
 
 # fit a linear regression model
 fit2_1_lm <- lm(Price ~ SqFt, data = data)
 summary(fit2_1_lm)
-
-# output
-#Call:
-#lm(formula = Price ~ SqFt, data = data)
-
-#Residuals:
-#     Min       1Q   Median       3Q      Max 
-#-1041.43   -99.12     1.99    59.26   751.43 
-
-#Coefficients:
-#            Estimate Std. Error t value Pr(>|t|)    
-#(Intercept) 69.30663   65.13461   1.064     0.29    
-#SqFt         0.60457    0.03713  16.282   <2e-16 ***
-#---
-#Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 
 minimal_x <- min(data$SqFt)
 maximum_x <- max(data$SqFt)
@@ -41,52 +21,82 @@ data_2_1 <- data.frame(SqFt = x_2_1_lm, Price = y_2_1_lm)
 
 g_2_1 + geom_line(data = data_2_1, aes(x = x_2_1_lm, y = y_2_1_lm), color = "red") 
 
-# fitting this model using a straight line is not too bad,but not perfect.
-# since at the bottom of the plot, more points are here and on the top of 
-# the plot, less points are here.
-
 ################################## (2.2) #######################################
-# We will try a new model
-# Price = b + a_{1} * SqFt + a_{2} * (SqFt −c)1_{SqFt>c}
+rss_fun <- function(par,SqFt,Price) {
 
-# Fit the model using optim function
-# We will use the least square method to fit the model
-
-# define the function
-# x is the vector of parameters
-# x[1] is b
-# x[2] is a_{1}
-# x[3] is a_{2}
-# x[4] is c
-# y is the vector of response
-# x is the vector of predictor
-# n is the number of observations
-# return the sum of square of residuals
-f_2_2 <- function(x, y, x, n) {
-    b <- x[1]
-    a_1 <- x[2]
-    a_2 <- x[3]
-    c <- x[4]
-    sum <- 0
-    for (i in 1:n) {
-        sum <- sum + (y[i] - b - a_1 * x[i] - a_2 * (x[i] - c) * (x[i] > c))^2
-    }
-    return(sum)
+  a1 <- par[1]
+  a2 <- par[2]
+  b <- par[3]
+  c <- par[4]
+  
+  predicted <- ifelse(SqFt > c, b + a1 * SqFt + a2 * (SqFt - c), b + a1 * SqFt)
+  #predicted <- b + a1 * SqFt + a2 * (SqFt - c)
+  
+  rss <- sum((Price - predicted)^2)
+  return(rss)
 }
 
-# fit the model
-# we will use the optim function to find the parameters
+c_func <- function(c, , Price) {
+  # Init
+  init_pars <- c(2, 0.1, 50, c)
 
-# init the parameters
-x_2_2 <- c(0, 0, 0, 0)
-# init the control parameters
-control <- list(fnscale = -1, maxit = 1000)
-# fit the model
-fit2_2 <- optim(x_2_2, f_2_2, y = data$Price, x = data$SqFt, n = length(data$Price), control = control)
+  # Optimize using optim()
+  result <- optim(par = init_pars, rss_fun, SqFt = SqFt, Price = Price)
+
+  opt_pars <- result$par
+  rss <- result$value
+
+  return(list(c = c, params = opt_pars, rss = rss))
+}
+
+
+c <- 150
+results <- c_func(c, SqFt = data$SqFt, Price = data$Price)
+results
+
 
 
 ################################## (2.3) #######################################
 
+stat1 <- function(data,vn){
+    data = as.data.frame(data[vn,])
+    res <- c_func(c, SqFt = data$SqFt, Price = data$Price)
+    return(res$c)
+}
+
+set.seed(12345)
+res1 = boot(data, stat1, R=1000)
+
+print(boot.ci(res1)) # , type = c("perc", "bca", "basic")
+
+# plot
+plot(res1)
+
 ################################## (2.4) #######################################
 
-################################## (2.5) #######################################
+# Jackknife Function
+jackknife <- function(data, fun) {
+  n <- length(data)
+  indices <- 1:n
+  estimates <- numeric(n)
+  
+  for (i in 1:n) {
+    jackknife_sample <- data[-i]
+    estimates[i] <- fun(jackknife_sample)
+  }
+  
+  bias_correction <- (n - 1) * mean(estimates) - fun(data)
+  
+  jackknife_var <- ((n - 1) / n) * sum((estimates - mean(estimates) - bias_correction)^2)
+  
+  return(list(estimates = estimates, jackknife_var = jackknife_var))
+}
+
+
+jackknife_results <- jackknife(c_values, function(c_val) {
+  c_func(c_val, SqFt, Price)$params[1]
+})
+
+# Print Jackknife Results
+cat("Jackknife Estimates of c:", jackknife_results$estimates, "\n")
+cat("Jackknife Variance of c:", jackknife_results$jackknife_var, "\n")
---
+++ Homework/Lab/Lab6/Playground2.R
@@ -0,0 +1,49 @@
+# EM algorithm
+
+library(ggplot2)
+
+
+# Question 1(DONE)
+
+# Load data
+data <- read.csv("./Homework/Lab/Lab6/censoredproc.csv", 
+                sep = ";", header = TRUE)
+
+
+
+# Since we don't use the failure row, so all the rows with cens = 1 are filtered
+left_data <- data[data$cens == 2,]
+
+layout(matrix(c(1:2), 1, 2))
+
+# plot the data
+hist(data$time, breaks = 100)
+
+# plot the left data
+hist(left_data$time, breaks = 100)
+
+# comment:
+# Accoridng to the plots , we can see that the left_data get sparser and not
+# converge to 0,  as what we saw at the plot 1.
+
+# Question 2 & 3
+
+# Since the first one is come from an exponential distribution, and second plot 
+# from a truncated exponential distribution. 
+
+# according to the page:
+# https://www.statlect.com/fundamentals-of-statistics/exponential-distribution-maximum-likelihood
+
+# we should know the formula 
+
+
+# question 4 
+# and according to the code that provided by the teacher, we can copy it and then
+# modify it to fit our data.
+
+# Question 5
+# Plot the density curve of the exp(ˆλ)  over plot 1
+
+# question 6
+# use se a parametric bootstrap to implement a function and then compare it with
+# the data we generated, and then write some comment.
