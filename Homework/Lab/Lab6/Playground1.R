34d361ed0b4c9bdaf76961cf9fa8e2bdb862f0e6
Pierre Qi <qiqinyuan@gmail.com>
2023-12-22 18:02:42 +0100

    update

--- Homework/Lab/Lab6/Playground1.R
+++ Homework/Lab/Lab6/Playground1.R
@@ -1,48 +1,143 @@
 # set random seed
 set.seed(12345)
 
-crossover1 <- function(config1, config2, p = 4){
+#DONE
+crossover <- function(config1, config2, p = 4){
   ncol <- length(config1)
   child_config <- config1
   child_config[p + 1:ncol] <- config2[1:p]
   return(child_config)
 }
 
-mutate <- function(config) {
+# (x,y) encoding, change y only
+mutate1 <- function(config) {
+  ncol <- length(config)
+  mutated_config <- config
+  queen_to_mutate <- sample(1:ncol, 1)
+  new_position <- mutated_config[queen_to_mutate]
+  new_y <- sample(1:ncol, 1)
+  new_position[2] <- new_y
+  mutated_config[queen_to_mutate] <- new_position
+  return(mutated_config)
+}
 
+# (binary) encoding, change one bit randomly
+mutate2 <- function(config) {
+  ncol <- length(config)
+  mutated_config <- config
+  queen_to_mutate <- sample(1:ncol, 1)
+  queen_integer <- mutated_config[queen_to_mutate]
+
+  num_bits <- log2(queen_integer) %/% 1 + 1
+  
+  # Randomly select a bit position to flip
+  bit_position <- sample(0:(num_bits - 1), 1)
+  
+  # Use bitwise XOR to flip the selected bit
+  flipped_integer <- bitwXor(queen_integer, 2^bit_position)
+  
+  return(flipped_integer)
+}
+
+
+# (col location) encoding, change col number only
+mutate3 <- function(config) {
+  ncol <- length(config)
+  mutated_config <- config
+  queen_to_mutate <- sample(1:ncol, 1)
+  new_position <- mutated_config[queen_to_mutate]
+  new_y <- sample(1:ncol, 1)
+  new_position[2] <- new_y
+  mutated_config[queen_to_mutate] <- new_position
+  return(mutated_config)
 }
 
 # encoding (X,Y) represent the position of the queen
 fitness1 <- function(config) {
-  # we will make a list of 
+   return(fitness(config))
 }
 
 
 # encoding (X) represent a number whose binary string shows 
 # the position of the queen in the current row
+
+find_set_bits_indices <- function(n) {
+  if (n == 0) {
+    return(NULL) 
+  }
+  
+  bit_indices <- integer(0)
+  bit_index <- 1
+  
+  while (n > 0) {
+    if ((n & 1) == 1) {
+      bit_indices <- c(bit_indices, bit_index)
+    }
+    n <- bitshift(n, -1)
+    bit_index <- bit_index + 1
+  }
+  
+  return(bit_indices)
+}
+
 fitness2 <- function(config) {
-  return(0)
+  new_config <- data.frame(x = c(), y = c())
+  row_number <- nrow(config)
+  for(i in 1:row_number){
+    number <- config[i]
+    queen_pos <- find_set_bits_indices(number)
+    queen_pos <- row_number - queen_pos
+    new_config <- rbind(new_config, c(i,config[i]))
+  }
+  return(fitness(new_config))
 }
 
 # encoding using a vector of length N, where N is the number of rows or columns
 # n-th element of the vector represents the column of the queen, since we assume
 # that only queen can be placed on a row. 
 
+# DONE
 fitness3 <- function(config) {
-  return(fitness(config))
+  new_config <- data.frame(x = c(), y = c())
+  queen_num <- nrow(config)
+  for(i in 1:queen_num){
+    new_config <- rbind(new_config, c(i,config[i]))
+  }
+  return(fitness(new_config))
 }
 
 # our fitness function to handle all the encodings, other encodings will be 
-# converted to this encoding.
+# converted to this encoding.(X,Y) Location Encoding 
 
-fitness <- function(p) {
-  ## more than one Q on a column?
-  result <- sum(duplicated(p)) +
-    ## more than one Q on a diagonal?
-    sum(duplicated(p - seq_along(p))) +
-    ## more than one Q on a reverse diagonal?
-    sum(duplicated(p + seq_along(p)))
-  return(result)
+# check attack between queen position
+is_attack <- function(queen1, queen2) {
+  return(
+    queen1$x == queen2$x || queen1$y == queen2$y || 
+    abs(queen1$x - queen2$x) == abs(queen1$y - queen2$y)
+  )
+}
+
+# check whole config is valid or not and return 
+# attacked queens number and unattacked queens number
+fitness <- function(config) {
+  queen_num <- nrow(config)
+  attacked_queens <- c()
+
+  for (i in 1:(queen_num - 1)) {
+    for (j in (i + 1):queen_num) {
+      if (is_attack(config[i,], config[j,])) {
+        attacked_queens <- c(attacked_queens, i, j)
+      }
+    }
+  }
+  
+  unattacked_queens <- setdiff(1:queen_num, unique(attacked_queens))
+  num_unattacked_queens <- length(unattacked_queens)
+  valid <- (length(unique(attacked_queens)) == 0)
+
+  return(list(valid = valid,
+              num_unattacked_queens, 
+              num_attacked_queens = 1 - num_unattacked_queens))
 }
 
 # (x,y) encoding
@@ -64,7 +159,6 @@ init_configuration_1 <- function(board_size = 8) {
 }
 
 # binary encoding
-# TODO: need to fix
 # if board_size = 8, then the max n is 2^8 - 1 = 255
 init_configuration_2 <- function(board_size = 8) {
   max_value <- 2^board_size - 1
@@ -155,31 +249,44 @@ print_board <- function(configuration, method = 1) {
 }
 
 board_size <- 8
-p_val <- 6
+
+# this p value shuld less equal than board_size/2
+p_val <- 4
 
 configuration <- init_configuration_1(board_size)
-configuration
-print_board(configuration, 1)
+# configuration
+# print_board(configuration, 1)
 
 configuration <- init_configuration_2(board_size)
-configuration
-print_board(configuration, 2)
-
+#configuration
+#print_board(configuration, 2)
 
 configuration3_1 <- init_configuration_3(board_size)
-configuration3_1
 
-configuration3_2 <- init_configuration_3(board_size)
-configuration3_2
+configuration3_2 <- NULL
 
-min_val <- fitness3(configuration)
-while(min_val != 0) {
+val_3_1 <- fitness3(configuration)
+
+while(!val_3_1.b.valid) {
+
+  if (configuration3_2 = NULL){
+    configuration3_2 <- init_configuration_3(board_size)    
+    val_3_2 <- mutate3(configuration3_2)
+  }
+  order(..., na.last = TRUE, decreasing = FALSE)
   # cross over
-  crossover_config <- crossover_configuration(configuration3_1, configuration3_2， p = p_val)
+  child_config <- crossover(configuration3_1, 
+                            configuration3_2， 
+                            p = p_val)
  
+  mutated_config <- mutate3(child_config)
+
   val <- fitness3(configuration)
-  if (val < min_val) {
-    min_val <- val
+  if (val.valid){
+    break
+  }
+  if (val.num_unattacked_queens < min_val_3) {
+    min_val_3 <- val
     print(min_val)
   }
 }
